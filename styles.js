dd83a8c643a7cc2616a8ca89493386a51b3dffaa
Hoyt Jolly <hoytbiz@gmail.com>
2025-01-20 16:37:04 -0600

    Create styles.js

---
+++ styles.js
@@ -0,0 +1,219 @@
+styles = styles = {
+            base: {
+                fontFamily: 'Arial',
+                fontSize: 24,
+                color: '#ffffff',
+                depth: 1,
+                origin: 'tLeft',
+                align: 'left',
+            },
+            button: {
+                backgroundColor: '#000000',
+                align: 'center',
+                padding: {
+                    x: 8,
+                    y: 3
+                },
+                depth: 100,
+                origin: 'cCenter',
+                flash: '#555555',
+                upSound: 'pop',
+                border1: '1 #ffffff',
+            },
+            overlayButton: {
+                depth: 1001,
+            },
+            overlayControl: {
+                depth: 1001,
+                backgroundColor: '#990000',
+                border1: '1 #ffffff',
+                border2: '2 #990000',
+                padding: {
+                    x: 3,
+                    y: 1
+                },
+            },
+            unPause: {
+                depth: 10001,
+                background: '#003300',
+                padding: {
+                    x: 20,
+                    y: 20
+                },
+            },
+
+            shopButtonLocked: {
+                depth: 2001,
+                backgroundColor: '#222222',
+                fill: '#ffffff',
+                origin: 'cCenter',
+                border1: '1 #ffffff',
+                upSound: 'thunk'
+            },
+            shopButtonBought: {
+                depth: 2001,
+                backgroundColor: '#000000',
+                fill: '#333333',
+                origin: 'cCenter',
+                border1: '1 #ffffff',
+                upSound: 'thunk'
+            },
+            shopButtonPoor: {
+                depth: 2001,
+                backgroundColor: '#990000',
+                fill: '#ffffff',
+                origin: 'cCenter',
+                border1: '1 #ffffff',
+                upSound: 'thunk'
+            },
+            shopButtonBuy: {
+                depth: 2001,
+                backgroundColor: '#007700',
+                origin: 'cCenter',
+                border1: '1 #ffffff',
+            },
+
+            overlayText: {
+                depth: 1001,
+                origin: 'cLeft'
+            },
+            overlayDialog: {
+                depth: 1001,
+                origin: 'tLeft',
+                fontSize: 32
+            },
+            overlayTextCentered: {
+                depth: 1001,
+                origin: 'cCenter'
+            },
+            overlayLocked: {
+                depth: 1001,
+                origin: 'cLeft',
+                fill: '#777777'
+            },
+            overlayBought: {
+                depth: 1001,
+                origin: 'cLeft',
+                fill: '#009900'
+            },
+            overlayPoor: {
+                depth: 1001,
+                origin: 'cLeft',
+                fill: '#990000'
+            },
+            messages: {
+                fill: '#000000',
+                backgroundColor: '#ffffff',
+                fontFamily: 'Georgia',
+                align: 'center',
+                origin: 'tRight',
+                depth: 2000,
+                padding: {
+                    x: 10,
+                    y: 10
+                }
+            },
+            floatingText: {
+                fill: '#ffffff',
+                stroke: '#000000',
+                strokeThickness: 3,
+                origin: 'cCenter',
+                depth: 999,
+                fontSize: 20,
+            },
+            titleScreenTitle: {
+                fontSize: '64px',
+                fontStyle: 'bold',
+                fill: '#22ff00',
+                stroke: '#000000',
+                strokeThickness: 6,
+                origin: 'tCenter',
+                depth: 1001
+            },
+            gameOverTitle: {
+                fontSize: '64px',
+                fontStyle: 'bold',
+                fill: '#ff8800',
+                stroke: '#000000',
+                strokeThickness: 6,
+                origin: 'tCenter',
+                depth: 1001
+            },
+            gameOverSubTitle: {
+                fontSize: '28px',
+                fontStyle: 'bold',
+                fill: '#ff8800',
+                stroke: '#000000',
+                strokeThickness: 2,
+                origin: 'tCenter',
+                depth: 1001
+            },
+
+            gameWonTitle: {
+                fontSize: '64px',
+                fontStyle: 'bold',
+                fill: '#ffffed',
+                stroke: '#000000',
+                strokeThickness: 6,
+                origin: 'tCenter',
+                depth: 1001
+            },
+            gameWonSubTitle: {
+                fontSize: '28px',
+                fontStyle: 'bold',
+                fill: '#ffffed',
+                stroke: '#000000',
+                strokeThickness: 2,
+                origin: 'tCenter',
+                depth: 1001
+            },
+            highScore: {
+                align: 'center',
+                origin: 'tCenter',
+                depth: 1001
+            },
+            overlayTitle: {
+                align: 'center',
+                origin: 'cCenter',
+                depth: 1001,
+                fontSize: '36px',
+                fontStyle: 'bold',
+            },
+            overlaySubTitle: {
+                align: 'center',
+                origin: 'cCenter',
+                depth: 1001,
+                fontSize: '32px',
+                fontStyle: 'bold',
+            },
+            overlaySubSubTitle: {
+                align: 'center',
+                origin: 'cCenter',
+                depth: 1001,
+                fontSize: '24px',
+                fontStyle: 'bold',
+            },
+            achievementsGreenLeft: {
+                fill: '#00ff00',
+                depth: 1001,
+                origin: 'cLeft',
+            },
+            achievementsRedLeft: {
+                fill: '#ff0000',
+                depth: 1001,
+                origin: 'cLeft',
+            },
+            achievementsGreenRight: {
+                fill: '#00ff00',
+                depth: 1001,
+                origin: 'cRight',
+                align: 'cRight'
+            },
+            achievementsRedRight: {
+                fill: '#ff0000',
+                depth: 1001,
+                origin: 'cRight',
+                align: 'cRight'
+            }
+
+        };
